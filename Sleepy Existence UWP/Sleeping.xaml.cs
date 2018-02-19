6e222c20676c6079a98a0174d58d89f098adc424
James Ross <git@james-ross.co.uk>
2018-02-19 22:54:07 +0000

    Finish tracking screen for now, add save button which works

--- Sleepy Existence UWP/Sleeping.xaml.cs
+++ Sleepy Existence UWP/Sleeping.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Credentials;
+using Windows.System.Display;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,16 +27,14 @@ namespace Sleepy_Existence
     /// </summary>
     public sealed partial class Sleeping : Page
     {
-        const string FormatTimer = @"hh\:mm";
         const string FormatTime = "HH:mm";
 
-        Brush ForegroundText = null;
-
-        DateTimeOffset InBed;
-        DateTimeOffset Awake;
-        DateTimeOffset OutOfBed;
+        DisplayRequest DisplayRequest;
+        DateTimeOffset InBed = DateTimeOffset.MinValue;
+        DateTimeOffset Awake = DateTimeOffset.MinValue;
+        DateTimeOffset OutOfBed = DateTimeOffset.MinValue;
         int TimeToFallAsleepM;
-        bool IsSleeping;
+        int Interruptions;
 
         DispatcherTimer Timer = new DispatcherTimer();
 
@@ -39,17 +42,30 @@ namespace Sleepy_Existence
         {
             this.InitializeComponent();
 
-            ForegroundText = textBlockTimer.Foreground;
+            DisplayRequest = new DisplayRequest();
 
             Timer.Tick += Timer_Tick;
-            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Interval = new TimeSpan(0, 0, 61 - DateTimeOffset.Now.Second);
             Timer.Start();
+
+            InBed = DateTimeOffset.Now;
 
             UpdateDisplay();
         }
 
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DisplayRequest.RequestActive();
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DisplayRequest.RequestRelease();
+        }
+
         private void Timer_Tick(object sender, object e)
         {
+            Timer.Interval = new TimeSpan(0, 0, 61 - DateTimeOffset.Now.Second);
             UpdateDisplay();
         }
 
@@ -57,23 +73,26 @@ namespace Sleepy_Existence
         {
             var asleep = InBed.AddMinutes(TimeToFallAsleepM);
 
-            textBlockTimer.Text = (IsSleeping ? DateTimeOffset.Now - InBed : OutOfBed - InBed).ToString(FormatTimer);
+            textBlockClock.Text = DateTimeOffset.Now.ToString(FormatTime);
+
             textBlockInBed.Text = InBed.ToString(FormatTime);
             textBlockAsleep.Text = asleep.ToString(FormatTime);
             textBlockAwake.Text = Awake.ToString(FormatTime);
             textBlockOutOfBed.Text = OutOfBed.ToString(FormatTime);
+            textBlockInterruptions.Text = Interruptions.ToString();
 
-            buttonSetInBed.IsEnabled = !IsSleeping;
-            buttonSetAwake.IsEnabled = IsSleeping;
-            buttonSetOutOfBed.IsEnabled = IsSleeping;
+            buttonSave.IsEnabled = InBed != DateTimeOffset.MinValue && Awake != DateTimeOffset.MinValue && OutOfBed != DateTimeOffset.MinValue;
         }
 
-        private void buttonSetInBed_Click(object sender, RoutedEventArgs e)
+        private void buttonInBedPlus_Click(object sender, RoutedEventArgs e)
         {
-            InBed = DateTimeOffset.Now;
-            Awake = DateTimeOffset.MinValue;
-            OutOfBed = DateTimeOffset.MinValue;
-            IsSleeping = true;
+            InBed = InBed.AddMinutes(1);
+            UpdateDisplay();
+        }
+
+        private void buttonInBedMinus_Click(object sender, RoutedEventArgs e)
+        {
+            InBed = InBed.AddMinutes(-1);
             UpdateDisplay();
         }
 
@@ -91,17 +110,104 @@ namespace Sleepy_Existence
             UpdateDisplay();
         }
 
-        private void buttonSetAwake_Click(object sender, RoutedEventArgs e)
+        private void buttonAwakePlus_Click(object sender, RoutedEventArgs e)
         {
-            Awake = DateTimeOffset.Now;
+            if (Awake == DateTimeOffset.MinValue)
+                Awake = DateTimeOffset.Now;
+            else
+                Awake = Awake.AddMinutes(1);
             UpdateDisplay();
         }
 
-        private void buttonSetOutOfBed_Click(object sender, RoutedEventArgs e)
+        private void buttonAwakeMinus_Click(object sender, RoutedEventArgs e)
         {
-            OutOfBed = DateTimeOffset.Now;
-            IsSleeping = false;
+            if (Awake == DateTimeOffset.MinValue)
+                Awake = DateTimeOffset.Now;
+            else
+                Awake = Awake.AddMinutes(-1);
             UpdateDisplay();
+        }
+
+        private void buttonOutOfBedPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (OutOfBed == DateTimeOffset.MinValue)
+                OutOfBed = DateTimeOffset.Now;
+            else
+                OutOfBed = OutOfBed.AddMinutes(1);
+            UpdateDisplay();
+        }
+
+        private void buttonOutOfBedMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (OutOfBed == DateTimeOffset.MinValue)
+                OutOfBed = DateTimeOffset.Now;
+            else
+                OutOfBed = OutOfBed.AddMinutes(-1);
+            UpdateDisplay();
+        }
+
+        private void buttonInterruptionPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Interruptions++;
+            UpdateDisplay();
+        }
+
+        private void buttonInterruptionMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Interruptions--;
+            if (Interruptions < 0)
+                Interruptions = 0;
+            UpdateDisplay();
+        }
+
+        private async void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var vault = new PasswordVault();
+            var ExistAccessToken = vault.Retrieve("exist.io", "access_token");
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {ExistAccessToken.Password}");
+
+            var jsonDate = JsonValue.CreateStringValue(DateTimeOffset.Now.ToString("yyyy-MM-dd"));
+            var asleep = InBed.AddMinutes(TimeToFallAsleepM);
+            var content = new HttpStringContent(new JsonArray() {
+                new JsonObject() {
+                    { "name", JsonValue.CreateStringValue("time_in_bed") },
+                    { "date", jsonDate },
+                    { "value", JsonValue.CreateStringValue((OutOfBed - InBed).TotalMinutes.ToString("F0")) }
+                },
+                new JsonObject() {
+                    { "name", JsonValue.CreateStringValue("sleep") },
+                    { "date", jsonDate },
+                    { "value", JsonValue.CreateStringValue((Awake - asleep).TotalMinutes.ToString("F0")) }
+                },
+                new JsonObject() {
+                    { "name", JsonValue.CreateStringValue("sleep_start") },
+                    { "date", jsonDate },
+                    { "value", JsonValue.CreateStringValue((asleep.TimeOfDay.TotalMinutes - 12 * 60).ToString("F0")) }
+                },
+                new JsonObject() {
+                    { "name", JsonValue.CreateStringValue("sleep_end") },
+                    { "date", jsonDate },
+                    { "value", JsonValue.CreateStringValue(Awake.TimeOfDay.TotalMinutes.ToString("F0")) }
+                },
+                new JsonObject() {
+                    { "name", JsonValue.CreateStringValue("sleep_awakenings") },
+                    { "date", jsonDate },
+                    { "value", JsonValue.CreateStringValue(Interruptions.ToString("F0")) }
+                },
+            }.Stringify(), Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json");
+
+            var response = await client.PostAsync(new Uri("https://exist.io/api/1/attributes/update/"), content);
+            if (!response.IsSuccessStatusCode)
+            {
+                await new MessageDialog(response.RequestMessage.RequestUri.ToString(), response.StatusCode.ToString()).ShowAsync();
+                return;
+            }
+
+            var jsonResponse = JsonValue.Parse(await response.Content.ReadAsStringAsync());
+            // TODO: Check result
+
+            this.Frame.GoBack();
         }
     }
 }
